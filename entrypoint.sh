a509ee047218309f999bdaf20de6e31f125f65cb
Jonas Birmé <jonas.birme@eyevinn.se>
2024-11-05 15:23:16 +0100

    fix: origin url

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,12 @@ if [ -z "$VAST_ENDPOINT" ]; then
   exit 1
 fi
 
-if [ -z "$ORIGIN_HOST" ]; then
-  echo "ORIGIN_HOST is required"
-  exit 1
-fi
-
-if [ -z "$PLAYLIST_PATH" ]; then
-  echo "PLAYLIST_PATH is required"
+if [ -z "$ORIGIN_URL" ]; then
+  echo "ORIGIN_URL is required"
   exit 1
 fi
 
 /app/ad_proxy 0.0.0.0 ${PORT:-8080} \
-  ${ORIGIN_HOST} ${ORIGIN_PORT:-443} \
-  ${PLAYLIST_PATH} \
-  ${VAST_ENDPOINT} -a advanced -i ${INSERTION_MODE:-static}
+  ${ORIGIN_URL} \
+  ${VAST_ENDPOINT} -a advanced -i ${INSERTION_MODE:-static} \
+  --interstitials-address https://${OSC_HOSTNAME}
