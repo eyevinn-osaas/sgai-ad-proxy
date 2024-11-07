26cf7fb6131a500fc1c265d01467be3f545c5626
Jonas Birmé <jonas.birme@gmail.com>
2024-11-07 09:30:49 +0100

    feat: made available for Eyevinn Open Source Cloud (#2)
    * feat: prepare for OSC
    
    * fix: origin url
    
    * fix: provide base url for interstitials address
    
    * chore: added OSC banner
    
    * fix: omit the advanced mode

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -z "$VAST_ENDPOINT" ]; then
+  echo "VAST_ENDPOINT is required"
+  exit 1
+fi
+
+if [ -z "$ORIGIN_URL" ]; then
+  echo "ORIGIN_URL is required"
+  exit 1
+fi
+
+/app/ad_proxy 0.0.0.0 ${PORT:-8080} \
+  ${ORIGIN_URL} \
+  ${VAST_ENDPOINT} -i ${INSERTION_MODE:-static} \
+  --interstitals-address https://${OSC_HOSTNAME}
