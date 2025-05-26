26f3f261cb21fe70cc2642179def6c3f0c9c2942
Jonas Birmé <jonas.birme@eyevinn.se>
2025-05-26 12:14:40 +0200

    fix: removed deprecated options

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,27 +10,8 @@ if [ -z "$ORIGIN_URL" ]; then
   exit 1
 fi
 
-COUCHDB_OPTS=""
-
-if [! -z "$COUCHDB_ENDPOINT" ]; then
-  if [ -z "$COUCHDB_USER" ]; then
-    echo "COUCHDB_USER is required"
-    exit 1
-  fi
-  if [ -z "$COUCHDB_PASSWORD" ]; then
-    echo "COUCHDB_PASSWORD is required"
-    exit 1
-  fi
-  if [ -z "$COUCHDB_TABLE" ]; then
-    echo "COUCHDB_TABLE is required"
-    exit 1
-  fi
-  COUCHDB_OPTS="--couchdb-endpoint $COUCHDB_ENDPOINT --couchdb-table $COUCHDB_TABLE" 
-fi
-
 /app/ad_proxy 0.0.0.0 ${PORT:-8080} \
   ${ORIGIN_URL} \
   ${VAST_ENDPOINT} \
   --ad-insertion-mode ${INSERTION_MODE:-static} \
-  --interstitials-address https://${OSC_HOSTNAME} \
-  ${COUCHDB_OPTS}
+  --interstitials-address https://${OSC_HOSTNAME}
